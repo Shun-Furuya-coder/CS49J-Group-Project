bea242f60d8810b337af69654375f9a184b4d4e6
ttrinh10 <116591095+ttrinh10@users.noreply.github.com>
2022-12-06 22:24:57 -0800

    Update Book.java

--- Book.java
+++ Book.java
@@ -12,23 +12,46 @@ public abstract class Book {
      this.author = author;
      this.numOfCopies = numOfCopies;
  }
+ /**
+   * Gets the name of the book
+   *
+   * @return the book name
+  **/
 
  public String getBookName() {
      return bookName;
  }
-
+/**
+ * Gets the name of the author
+ *
+ *@return the author of the book
+**/
  public String getAuthor() {
      return author;
  }
+ /**
+ * Gets the number of copies
+ *
+ * @return the number of copies
+ **/
 
  public int getNumOfCopies() {
      return numOfCopies;
  }
-
+ 
+ /**
+ *Sets the number of copies
+ *
+ * @param numOfCopies the number of copies
+ *
+ **/
  public void setNumOfCopies(int numOfCopies) {
      this.numOfCopies = numOfCopies;
  }
+ 
 
  public abstract void borrowBook(Borrower borrower);
  public abstract void returnBook(Borrower borrower);
+
+
 }
